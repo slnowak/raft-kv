fc241f5fd9eb4a53167dfab1c7b30962dc6f8d25
novy <slnowak154@gmail.com>
2016-11-12 17:02:29 +0100

    Project setup

---
+++ src/main/java/pl/edu/agh/raftkv/Main.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.raftkv;
+
+/**
+ * Created by novy on 12.11.16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/test/groovy/pl/edu/agh/raftkv/DummyTest.groovy
@@ -0,0 +1,14 @@
+package pl.edu.agh.raftkv
+
+import spock.lang.Specification
+
+/**
+ * Created by novy on 12.11.16.
+ */
+class DummyTest extends Specification {
+
+    def "should pass"() {
+        expect:
+        true
+    }
+}
